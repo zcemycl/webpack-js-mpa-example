e33f4595edf5893f1fbcc20ccb5649dcd4848bd5
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-12 16:56:24 +0100

    update

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,9 @@ console.log(__dirname)
 console.log(`file://${__dirname.slice(0, si+length)}/dist/index.html`)
 jest.setTimeout(30000)
 
+const ci = Boolean(process.env.CI || false);
+console.log('ci '+ci)
+
 test('should click', async () => {
     const browser = await puppeteer.launch({
         headless: true, 
@@ -13,10 +16,13 @@ test('should click', async () => {
         args: ['--window-size=1920,1080']
     });
     const page = await browser.newPage();
-    await page.goto(`file://${__dirname.slice(0, si+length)}/dist/index.html`);
+    // await page.goto(`file://${__dirname.slice(0, si+length)}/dist/index.html`);
+    await page.goto(ci?
+        'https://zcemycl.github.io/webpack-js-mpa-example':
+        `file://${__dirname.slice(0, si+length)}/dist/index.html`);
     await page.click('button');
     const paragraph = await page.$eval('p', (input) => input.innerHTML);
     console.log(paragraph)
     expect(paragraph).not.toBe('');
-    browser.close()
+    await browser.close()
 })
